c4bfaf5dda787d3da8f6c505e6d779dd3dc1ad69
mussoi <mussoidaniel@gmail.com>
2025-03-16 18:24:38 -0300

    feat: implementa opcao de subtracao no menu

--- menu.py
+++ menu.py
@@ -10,15 +10,17 @@ def menu():
 
     if escolha == "1":  
         num1 = float(input("Digite o primeiro número: "))  
-        num2 = float(input("Digite o segundo número: "))  
+        num2 = float(input("Digite o segundo número: "))
         resultado = calculadora.somar(num1, num2)  
-        print(f"Resultado: {resultado}")  
-    elif escolha == "0":  
+        print(f"Resultado: {resultado}") 
+    elif escolha == "0":
         return
-
-
-
-
+    elif escolha == "2":  
+        num1 = float(input("Digite o primeiro número: "))  
+        num2 = float(input("Digite o segundo número: "))  
+        resultado = calculadora.subtrair(num1, num2)  
+        print(f"Resultado: {resultado}")  
+        return
 
 if __name__ == "__main__":  
     menu()
