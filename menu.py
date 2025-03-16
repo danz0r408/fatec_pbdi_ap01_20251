17c27ef69b448ac4895dfbd93d726d0ab5d1a84c
mussoi <mussoidaniel@gmail.com>
2025-03-16 18:13:17 -0300

    feat: implementa estrutura inicial do menu

---
+++ menu.py
@@ -0,0 +1,24 @@
+import calculadora
+def menu():  
+    print("\n=== Calculadora ===")  
+    print("1. Somar")  
+    print("2. Subtrair")  
+    print("3. Multiplicar")  
+    print("4. Dividir")  
+    print("0. Sair")  
+    escolha = input("Escolha uma opção: ")  
+
+    if escolha == "1":  
+        num1 = float(input("Digite o primeiro número: "))  
+        num2 = float(input("Digite o segundo número: "))  
+        resultado = calculadora.somar(num1, num2)  
+        print(f"Resultado: {resultado}")  
+    elif escolha == "0":  
+        return
+
+
+
+
+
+if __name__ == "__main__":  
+    menu()
